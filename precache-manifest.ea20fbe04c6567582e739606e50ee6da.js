0fa15fd3720f95a7cb95c1975fd6ef0f87afebbd
Max Johansen <macsj200@gmail.com>
2019-03-15 22:30:56 -0600

    Add files via upload

---
+++ precache-manifest.ea20fbe04c6567582e739606e50ee6da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4efcf251d6f8ab09cba3",
+    "url": "/blockbattle-frontend/static/js/runtime~main.4efcf251.js"
+  },
+  {
+    "revision": "97f48945289c14c27fde",
+    "url": "/blockbattle-frontend/static/js/main.97f48945.chunk.js"
+  },
+  {
+    "revision": "12f8d1997933faefcea5",
+    "url": "/blockbattle-frontend/static/js/1.12f8d199.chunk.js"
+  },
+  {
+    "revision": "97f48945289c14c27fde",
+    "url": "/blockbattle-frontend/static/css/main.a1fdaae0.chunk.css"
+  },
+  {
+    "revision": "94a33702755d7eeea243f8b6deada4ea",
+    "url": "/blockbattle-frontend/index.html"
+  }
+];
